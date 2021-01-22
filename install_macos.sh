a4eedc0d2583a7f79d562d3c9d9a09af760e5f0b
Gil Cunha <gil.cunha@overcube.com>
2021-01-22 10:28:18 +0000

    Macos implementation ready

--- install_macos.sh
+++ install_macos.sh
@@ -2,6 +2,6 @@
 
 if ! command -v brew &> /dev/null
 then
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	python3 -m pip install --user ansible
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.shee
+	brew install ansible
 fi
