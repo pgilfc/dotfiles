68a3eafa62a4e676552b1a05f9e2d13a6d7ad199
Gil Cunha <gil.cunha@kelvininc.com>
2024-11-29 22:59:19 +0000

    feat(vim): updates

--- roles/vim/files/lazyvim/lua/config/options.lua
+++ roles/vim/files/lazyvim/lua/config/options.lua
@@ -5,7 +5,7 @@
 local opt = vim.opt
 
 opt.shiftwidth = 4 -- number of spaces to use for autoindent
-opt.tabstop = 4    -- number of spaces that a <Tab> in the file counts for
+opt.tabstop = 4 -- number of spaces that a <Tab> in the file counts for
 opt.softtabstop = 4
 
 opt.scrolloff = 10 -- number of screen lines above and below cursor
@@ -13,7 +13,7 @@ opt.scrolloff = 10 -- number of screen lines above and below cursor
 ---
 
 opt.swapfile = true -- enable swap files
-opt.backup = true   -- enable backup files
+opt.backup = true -- enable backup files
 opt.undofile = true -- enable undo files
 opt.directory = os.getenv("HOME") .. "/.vim/swapdir//"
 opt.backupdir = os.getenv("HOME") .. "/.vim/backupdir//"
@@ -22,4 +22,4 @@ opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 ---
 
 opt.incsearch = true -- incremental search
-opt.hlsearch = true  -- highlight search results
+opt.hlsearch = true -- highlight search results
