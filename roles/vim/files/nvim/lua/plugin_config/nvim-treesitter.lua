87aacbdc571e5678b2f32387ea2ce2ab8013f548
Gil <pgilfc@gmail.com>
2024-12-03 15:53:51 +0000

    feat: adapt packages to fedora

--- roles/vim/files/nvim/lua/plugin_config/nvim-treesitter.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- A list of parser names, or "all"
-    ensure_installed = { "c", "python", "lua", "go", "javascript", "typescript", "sql", "yaml", "json", "bash" },
-    sync_install = false,
-    auto_install = true,
-    highlight = { enable = true, additional_vim_regex_highlighting = false },
-    indent = { enable = true },
-}
