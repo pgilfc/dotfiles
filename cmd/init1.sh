0cb0bbe97ba31de0e8b84f4e9c984e72e5b6beb6
pgilfc <pgilfc@gmail.com>
2026-07-22 14:17:25 +0100

    fix: missing env configurations

--- cmd/init1.sh
+++ cmd/init1.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 ansible-playbook -i hosts playbook_os.yml --ask-become-pass
+# could also be
+# ansible-playbook -i hosts playbook_os.yml --ask-become-pass -e os_enroll_tpm=true
+# to enroll TPM on the OS level
